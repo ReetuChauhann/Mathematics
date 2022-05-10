0b42098666311371bc3bd11814591fc3b02c5b80
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-10 16:25:46 +0530

    set ith bit

---
+++ setithbit.java
@@ -0,0 +1,10 @@
+
+
+public class setithbit {
+   public static void main(String[] args) {
+    int n=20;
+    int fourthbit=n|(1<<(4-1)); // output is 28
+    System.out.println(fourthbit);
+   }
+    
+}
