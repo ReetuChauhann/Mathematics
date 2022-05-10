3a0ecf8f89c05d808ea3b114cd11b5b61094b15a
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-10 16:48:03 +0530

    negative of a number or 2s compliment

---
+++ complimentornegative.java
@@ -0,0 +1,7 @@
+public class complimentornegative {
+    public static void main(String[] args) {
+        int n=20;
+        int complimentornegativeofgivennumber=-n;
+        System.out.println(complimentornegativeofgivennumber);
+    }
+}
